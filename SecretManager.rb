d999fd98647b5330d4d69e6925d16567505f326c
msk6252 <msk6252@yahoo.co.jp>
2022-01-16 20:56:42 +0900

    add: SecretManagerのモジュールを追加

---
+++ SecretManager.rb
@@ -0,0 +1,20 @@
+require 'aws-sdk-secretsmanager'
+require 'logger'
+require 'json'
+
+class SecretManager
+  def initialize(secret_id)
+    @logger = Logger.new($stdout)
+    @secret_id = secret_id
+  end
+
+  def get_secret(key)
+    begin
+      scm = Aws::SecretManager::Client.new(region: "ap-northeast-1")
+      secret = scm.get_secret_value(secret_id: @secret_id)
+      return JSON.load(secret.secret_string)[key]
+    rescue => e
+      @logger.error(e)
+    end
+  end
+end
