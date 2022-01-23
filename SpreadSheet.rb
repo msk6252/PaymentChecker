81af130730ffe25c88ff3a013eee161a437732a0
msk6252 <msk6252@yahoo.co.jp>
2022-01-23 16:13:57 +0900

    add: SpreadSheetを操作するモジュールを追加

---
+++ SpreadSheet.rb
@@ -0,0 +1,37 @@
+require 'logger'
+require 'google_drive'
+require './SecretManager.rb'
+
+module SpreadSheet
+  def self.init(sheet_id, sheet_num)
+    begin
+      @logger = Logger.new($stdout)
+
+      session = GoogleDrive::Session.from_config("config.json")
+
+      return session.spreadsheet_by_key(sheet_id).worksheets[sheet_num]
+    rescue => e
+      @logger.error("[SpreadSheet] Initialize: #{e}")
+    end
+  end
+
+  def self.write(config = {})
+      puts config
+    begin
+      sheet_id = config[:sheet_id]
+      sheet_num = config[:sheet_num]
+      row = config[:row]
+      col = config[:col]
+      value = config[:value]
+
+      puts sheet_id, sheet_num, row, col, value
+
+      ws = init(sheet_id, sheet_num)
+
+      ws[row, col] = value
+      ws.save
+    rescue => e
+      @logger.error("[SpreadSheet] Initialize: #{e}")
+    end
+  end
+end
