5484241c1c5d925842ec14b165b265fe685f4b15
msk6252 <msk6252@yahoo.co.jp>
2022-01-30 18:57:39 +0900

    modify: モジュールからクラスに変更

--- SpreadSheet.rb
+++ SpreadSheet.rb
@@ -2,32 +2,15 @@ require 'logger'
 require 'google_drive'
 require './SecretManager.rb'
 
-module SpreadSheet
-  def self.init(sheet_id, sheet_num)
+class SpreadSheet
+  attr_accessor :ws
+  def initialize(sheet_id)
     begin
       @logger = Logger.new($stdout)
 
       session = GoogleDrive::Session.from_config("config.json")
 
-      return session.spreadsheet_by_key(sheet_id).worksheets[sheet_num]
-    rescue => e
-      @logger.error("[SpreadSheet] Initialize: #{e}")
-    end
-  end
-
-  def self.write(config = {})
-    return if config
-    begin
-      sheet_id = config[:sheet_id]
-      sheet_num = config[:sheet_num]
-      row = config[:row]
-      col = config[:col]
-      value = config[:value]
-
-      ws = init(sheet_id, sheet_num)
-
-      ws[row, col] = value
-      ws.save
+      @ws = session.spreadsheet_by_key(sheet_id)
     rescue => e
       @logger.error("[SpreadSheet] Initialize: #{e}")
     end
